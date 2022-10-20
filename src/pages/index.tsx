d84b47dc082d610fe7d4d69444326c2dd632b156
charlesponti <cj@ponti.io>
2022-10-19 22:04:52 -0700

    feat(Home): add design for landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,11 +15,27 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="container mx-auto flex min-h-screen flex-col pt-14">
-        <PageWrap>
-          <h1 className="text-5xl font-extrabold leading-normal text-gray-700 md:text-[5rem]">
-            Home
-          </h1>
+      <main>
+        <PageWrap className="mt-48 relative">
+          <div
+            aria-hidden="true"
+            className="absolute inset-0 grid grid-cols-2 -space-x-52 opacity-40 dark:opacity-20"
+          >
+            <div className="blur-[106px] h-56 bg-gradient-to-br from-primary to-purple-400 dark:from-blue-700" />
+            <div className="blur-[106px] h-32 bg-gradient-to-r from-cyan-400 to-sky-300 dark:to-indigo-600" />
+          </div>
+          <div className="max-w-7xl mx-auto px-6 md:px-12 xl:px-6">
+            <div className="ml-auto">
+              <div className="lg:w-4/5 text-center mx-auto">
+                <h1 className="text-gray-400 dark:text-white font-bold text-5xl md:text-6xl xl:text-7xl">
+                  Built for{" "}
+                  <span className="text-primary dark:text-white">
+                    imagineers.
+                  </span>
+                </h1>
+              </div>
+            </div>
+          </div>
         </PageWrap>
       </main>
     </>
